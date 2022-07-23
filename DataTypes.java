76ad4347b6d4553189e9bff918d5a4dcda235cac
PURNA1224 <sekhar1224@sasi.ac.in>
2022-07-23 18:33:29 +0530

    DataTypes file added by purna

---
+++ DataTypes.java
@@ -0,0 +1,23 @@
+class DataTypes
+{
+	public static void main(String args[])
+	{
+		byte a=1;
+		System.out.println("Byte Value is "+a);
+		int b=10;
+		System.out.println("Integer Value is "+b);
+		long c=255;
+		System.out.println("Long Value is "+c);
+		short d=48;
+		System.out.println("Short Value is "+d);
+		float e=8.0f;
+		System.out.println("Float Value is "+e);
+		double f=3.23;
+		System.out.println("Double Value is "+f);
+		char g='w';
+		System.out.println("Character Value is "+g);
+		boolean h=true;
+		System.out.println("Boolean Value is "+h);
+	}
+}
+
