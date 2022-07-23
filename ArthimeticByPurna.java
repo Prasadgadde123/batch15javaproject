cfcd48c5f37fd7708622f36b561537a416f58f34
PURNA1224 <sekhar1224@sasi.ac.in>
2022-07-23 18:22:36 +0530

    Arthimetic file added by Purna

---
+++ ArthimeticByPurna.java
@@ -0,0 +1,20 @@
+class ArthimeticByPurna
+{
+	public static void main(String args[])
+	{
+		int a=4,b=5,sum,sub,mul,rem;
+		float div;
+		sum=a+b;
+		sub=a-b;
+		mul=a*b;
+		div=a/b;
+		rem=a%b;
+	
+		System.out.println("Sum is "+sum);
+		System.out.println("Subtraction is "+sub);
+		System.out.println("Multiplication is "+mul);
+		System.out.println("Division is "+div);
+		System.out.println("Remainder is "+rem);
+
+	}
+}
