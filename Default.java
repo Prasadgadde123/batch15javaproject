debf8e8895ff486c025cae151e9d304b2248ca23
PURNA1224 <sekhar1224@sasi.ac.in>
2022-07-23 18:28:53 +0530

    Default file added by purna

---
+++ Default.java
@@ -0,0 +1,28 @@
+class Default 
+{
+	static byte a;
+	static int b;
+	static long c;
+	static short d;
+	static float e;
+	static double f;
+	static char g;
+	static boolean h;
+	
+	public static void main(String args[])
+	{
+		System.out.println("Byte Value is "+a);
+		System.out.println("Integer Value is "+b);
+		System.out.println("Long Value is "+c);
+		System.out.println("Short Value is "+d);
+		System.out.println("Float Value is "+e);
+		System.out.println("Double Value is "+f);
+		System.out.println("Character Value is "+g);
+		System.out.println("Boolean Value is "+h);
+	}
+}
+
+
+
+
+
